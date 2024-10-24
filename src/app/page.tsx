a0f6e8f534569bf8f63c33f4e192a39f88475046
Lê Hoàng Phúc <lehaongphuc733@gmail.com>
2024-10-24 12:30:51 +0700

    test1
    demo

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>Hello Bo
+      <div>
+        <Button type="primary">Click</Button>
+      </div>
+    </div>
+  );
+}
